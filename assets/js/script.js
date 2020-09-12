c0f209a7dc51de4ed8f0213afc18917cbb0a48bb
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-09-12 15:27:09 -0400

    started all over the code since I didn't like the direction it was going

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,36 @@
-var list = JSON.parse(localStorage.getItem('todolist')) || [];
+var searchButton = $(".btn");
 
-// Renders our to-dos to the page
-function renderTodos(list) {
-  // Empties out the html
-  $('#to-dos').empty();
+var apiUrl = "918084d8060d50df0258a5c8181d86ec";
 
-  // Iterates over the 'list'
-  for (var i = 0; i < list.length; i++) {
-    // Creates a new variable 'toDoItem' that will hold a "<p>" tag
-    // Sets the `list` item's value as text of this <p> element
-    var toDoItem = $('<p>');
-    toDoItem.text(list[i]);
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  console.log(searchHistory);
 
-    // Creates a button `toDoClose` with an attribute called `data-to-do` and a unique `id`
-    var toDoClose = $('<button>');
-    toDoClose.attr('data-to-do', i);
 
-    // Gives the button a class called 'checkbox'
-    toDoClose.addClass('checkbox');
 
-    // Adds a checkmark symbol as its text value
-    toDoClose.text('✓');
 
-    // Adds the button to the `toDoItem`
-    toDoItem = toDoItem.prepend(toDoClose);
+function loadHistory(){
+  for(var i = 0; i < searchHistory.length; i++){
 
-    // Adds 'toDoItem' to the To-Do List div
-    $('#to-dos').append(toDoItem);
+    var cityName = $(".list-group").addClass("list-group-item");
+    cityName.attr("data-index", searchHistory[i]);
+    cityName.append("<li>" + searchHistory + "</li>");
+    
   }
 }
 
-$('#add-to-do').on('click', function(event) {
-  event.preventDefault();
+var keyCount = 0;
 
-  // Get the to-do "value" from the textbox and store it as a variable using `.val()` and `.trim()`
-  var toDoTask = $('#to-do')
-    .val()
-    .trim();
+searchButton.click(function(){
 
-  // Add the new to-do to our local 'list' variable
-  list.push(toDoTask);
+  var searchInput = $(".searchInput").val();
 
-  // Update the to-dos on the page
-  renderTodos(list);
+  var cityName = $(".list-group").addClass("list-group-item");
+    cityName.attr("data-index", searchInput)
 
-  // Save the to-dos into localStorage
-  // We need to use JSON.stringify to turn the list from an array into a string
-  localStorage.setItem('todolist', JSON.stringify(list));
 
-  // Clear the textbox when done using `.val()`
-  $('#to-do').val('');
-});
+    // Variable for current weather working 
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiUrl + "&units=imperial";
+    // Variable for 5 day forecast working
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiUrl + "&units=imperial";
 
-$(document).on('click', '.checkbox', function() {
-  // Get the `id` of the button from its data attribute and hold in a variable called 'toDoNumber'
-  var toDoNumber = $(this).attr('data-to-do');
-
-  // Delete the to-do with that `id` from our local `list` using `.splice()`
-  list.splice(toDoNumber, 1);
-
-  // Update the to-dos on the page
-  renderTodos(list);
-
-  // Save the to-dos into localStorage
-  // We need to use JSON.stringify to turn the list from an array into a string
-  localStorage.setItem('todolist', JSON.stringify(list));
-});
-
-// render our to-dos on page load
-renderTodos(list);
+})
