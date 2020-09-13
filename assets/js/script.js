eba910e8239ff594790ae481230a3e8ffad33708
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-09-12 20:42:09 -0400

    Added a few more codes still struggling to add when the user press the previous history cities to display their weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,22 +15,6 @@ function loadHistory(){
     var cityName = $(".list-group").addClass("list-group-item");
     cityName.attr("data-index", searchHistory[i]);
     cityName.append("<li>" + searchHistory[i] + "</li>");
-
-    for (var i = 0; i < repos.length; i++) {
-      // format repo name
-      var cityName = repos[i].owner.login + "/" + repos[i].name;
-    
-      // create a container for each repo
-      var repoEl = document.createElement("div");
-      repoEl.classList = "list-item flex-row justify-space-between align-center";
-    
-      // create a span element to hold repository name
-      var titleEl = document.createElement("span");
-      titleEl.textContent = cityName;
-    
-      // append to container
-      repoEl.appendChild(titleEl);
-
     
   }
 }
