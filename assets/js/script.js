7deff30d43a1c66a5c13100fb80b367ffe7faf70
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-09-15 13:39:39 -0400

    for some reason my app doesnt work here is the same code hopefully it works now

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,7 +127,6 @@ var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
 
                 fiveDayDiv.append("<div class=weekendForecast>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
 
-
             })
 
         });
